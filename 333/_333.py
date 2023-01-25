c53ea82c9766e507d46e9a9cb0d9bccf0cc26552
opilane <opilane@HP-CZC2349HTG>
2023-01-25 10:53:05 +0200

    Add project files.

---
+++ 333/_333.py
@@ -0,0 +1,67 @@
+from random import *
+from math import *
+
+#############1#############
+#rand=randint(1,9)
+#print(rand)
+#while rand!=0:
+#    print("   -----")
+#    print("  |  O  |")
+#    print("  !  -  !")
+#    print("   -----")
+#    print()
+#    rand-=1
+
+#############2#############
+#x="x"
+#n=x
+#while type(n) != int:
+#    try:
+#        n = int(input("chislo ne privosho0dashee "))
+#    except:
+#        TypeError
+
+#g=x
+#while type (g)!=int:
+#    try:
+#        g = int(input("stepen "))
+#    except:
+#        TypeError
+
+#u=0
+#for i in range (1,n,1):
+#    u=i**g
+#    if u<n:
+#        print(u)
+#    else:
+#        break
+#print("konec\n")
+#############3#############
+
+
+#############4#############
+#d=0
+#xd=1
+#while d<=100:
+#    d=d+xd
+#    print(d)
+#    xd+=1
+
+#############5#############
+#l=0
+#x=0
+#print(x)
+#y=1
+#print(y)
+#z=x+y
+#print(z)
+#x=y
+#y=z
+#z=x+y
+#print(z)
+#while l!=10: 
+#    x=y
+#    y=z
+#    z=x+y
+#    print(z)
+#    l+=1
